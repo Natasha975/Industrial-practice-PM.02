b526ef0e75b1d355feb00b388a425e0bdb7d4982
Natasha975 <145749236+Natasha975@users.noreply.github.com>
2024-06-26 11:22:34 +0300

    Practice results
    Practice results: application and database script

---
+++ RoadsOfRussia App/RoadsOfRussia App/AllWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoadsOfRussia_App
+{
+	/// <summary>
+	/// Логика взаимодействия для AllWindow.xaml
+	/// </summary>
+	public partial class AllWindow : Window
+	{
+		public AllWindow()
+		{
+			InitializeComponent();
+		}
+		public string terr = "";
+		public string brush = "";
+		public string devis = "";
+		/// <summary>
+		/// Выбор сотрудников, которые работают на данной территории
+		/// </summary>
+		private void tviLevel0_Selected(object sender, RoutedEventArgs e)
+		{
+			e.Handled = true;
+			var sel = e.Source as TreeViewItem;
+			if (sel != null)
+			{
+				terr = sel.Header as String;
+;               using (var db = new RoadsOfRussiaEntities())
+				{
+					var users = from em in db.Employees
+								join ter in db.Territorys on em.IdTerritory equals ter.Id
+								join rol in db.Roles on em.IdRole equals rol.id
+								where ter.NameTerritory == terr
+								select new
+								{
+									em.Lastname,
+									em.Name,
+									em.Surname,
+									ter.NameTerritory,
+									ter.NameBranch,
+									ter.NameDivision,
+									em.Office,
+									em.E_mail
+								};
+					dgList.ItemsSource = users.ToList();
+				}
+			}
+		}
+		private void tviLevel1_Selected(object sender, RoutedEventArgs e)
+		{
+			e.Handled = true;
+			var sel = e.Source as TreeViewItem;
+			if (sel != null)
+			{
+				brush = sel.Header as String;
+				; using (var db = new RoadsOfRussiaEntities())
+				{
+					var users = from em in db.Employees
+								join ter in db.Territorys on em.IdTerritory equals ter.Id
+								join rol in db.Roles on em.IdRole equals rol.id
+								where ter.NameTerritory == terr && ter.NameBranch == brush
+								select new
+								{
+									em.Lastname,
+									em.Name,
+									em.Surname,
+									ter.NameTerritory,
+									ter.NameBranch,
+									ter.NameDivision,
+									em.Office,
+									em.E_mail
+								};
+					dgList.ItemsSource = users.ToList();
+				}
+			}
+		}
+		private void tviLevel2_Selected(object sender, RoutedEventArgs e)
+		{
+			e.Handled = true;
+			var sel = e.Source as TreeViewItem;
+			if (sel != null)
+			{
+				devis = sel.Header as String;
+				; using (var db = new RoadsOfRussiaEntities())
+				{
+					var users = from em in db.Employees
+								join ter in db.Territorys on em.IdTerritory equals ter.Id
+								join rol in db.Roles on em.IdRole equals rol.id
+								where ter.NameTerritory == terr && ter.NameBranch == brush && ter.NameDivision == devis
+								select new
+								{
+									em.Lastname,
+									em.Name,
+									em.Surname,
+									ter.NameTerritory,
+									ter.NameBranch,
+									ter.NameDivision,
+									em.Office,
+									em.E_mail
+								};
+					dgList.ItemsSource = users.ToList();
+				}
+			}
+		}
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			AllEmp();
+			using (var db = new RoadsOfRussiaEntities())
+			{
+				var newsNames = db.News.Select(n => n.Name).ToList();
+				lvNews.ItemsSource = newsNames;
+			}
+		}
+		public void AllEmp()
+		{
+			using (var db = new RoadsOfRussiaEntities())
+			{
+				var users = from em in db.Employees
+							join ter in db.Territorys on em.IdTerritory equals ter.Id
+							join rol in db.Roles on em.IdRole equals rol.id
+							select new
+							{
+								em.Lastname,
+								em.Name,
+								em.Surname,
+								ter.NameTerritory,
+								ter.NameBranch,
+								ter.NameDivision,
+								em.Office,
+								em.E_mail
+							};
+				dgList.ItemsSource = users.ToList();
+			}
+		}
+		private void btAllEmployees_Click(object sender, RoutedEventArgs e)
+		{
+			AllEmp();
+		}
+		/// <summary>
+		/// Реализация поиска по ФИО, названию отдела и почте
+		/// </summary>
+		private void tbPoisk_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			string textPoisk = tbPoisk.Text;
+			using (var db = new RoadsOfRussiaEntities())
+			{
+				var users = from em in db.Employees
+							join ter in db.Territorys on em.IdTerritory equals ter.Id
+							join rol in db.Roles on em.IdRole equals rol.id
+							where em.Lastname.Contains(textPoisk) || em.Name.Contains(textPoisk) || 
+								  em.Surname.Contains(textPoisk) || ter.NameDivision.Contains(textPoisk) ||
+								  em.E_mail.Contains(textPoisk)
+							select new
+							{
+								em.Lastname,
+								em.Name,
+								em.Surname,
+								ter.NameTerritory,
+								ter.NameBranch,
+								ter.NameDivision,
+								em.Office,
+								em.E_mail
+							};
+				dgList.ItemsSource = users.ToList();
+			}
+		}
+	}
+}
---
+++ RoadsOfRussia App/RoadsOfRussia App/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoadsOfRussia_App
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btVxod_Click(object sender, RoutedEventArgs e)
+		{
+			string log = tbLog.Text;
+			string pass = tbPass.Text;
+			using (var db = new RoadsOfRussiaEntities())
+			{
+				var empl = db.Employees.FirstOrDefault(em => em.Login == log && em.Password == pass);
+
+				if (empl != null)
+				{
+					AllWindow allWindow = new AllWindow();
+					allWindow.Show();
+					this.Hide();
+				}
+				else
+				{
+					MessageBox.Show("Неверный логин или пароль!");
+				}
+			}
+		}
+	}
+}
